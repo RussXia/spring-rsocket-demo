2de2ecee9cfb5ca25f1678fe9b44d85502319cfa
RuzzZZ <xzy199400@gmain.com>
2021-02-24 17:56:31 +0800

    adjust log level & encoder/decoder

--- rsocket-client/src/main/java/com/example/rsocket/client/config/RSocketClientConfiguration.java
+++ rsocket-client/src/main/java/com/example/rsocket/client/config/RSocketClientConfiguration.java
@@ -5,8 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.cbor.Jackson2CborDecoder;
-import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 import reactor.util.retry.Retry;
@@ -24,8 +24,8 @@ public class RSocketClientConfiguration {
     @Bean
     public RSocketStrategies rSocketStrategies() {
         return RSocketStrategies.builder()
-                .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
-                .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+                .encoders(encoders -> encoders.add(new Jackson2JsonEncoder()))
+                .decoders(decoders -> decoders.add(new Jackson2JsonDecoder()))
                 .build();
     }
 
